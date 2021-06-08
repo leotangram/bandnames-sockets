7afa507135feb6d5f6607259603cfb0fca8780be
Leonardo Omaña <leotangram@gmail.com>
2021-06-08 10:40:42 -0500

    First components

---
+++ src/components/BandAdd.js
@@ -0,0 +1,16 @@
+const BandAdd = () => {
+  return (
+    <>
+      <h3>Add band</h3>
+      <form>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="New name band"
+        />
+      </form>
+    </>
+  )
+}
+
+export default BandAdd
